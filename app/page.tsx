4f0a571b5b8029aa53cd145b0d9d99edbb3806c2
Cahya Yoga Ariyanto <cahyayoga10@gmail.com>
2026-01-30 23:56:29 +0700

    part 3

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,55 @@
-import About from "./pages/about/page";
+import Button from "@/app/components/button/Button";
+import Caption from "@/app/components/caption/Caption";
+import EducationFlow from "@/app/components/educationFlow/EducationFlow";
+import Icon from "@/app/components/icon/Icon";
+import SkillsSection from "@/app/components/skill/Skill";
 
-export default function HomePage(){
-    return(
-        <About/>
-    );
-};
+
+export default function About() {
+  return (
+    <>
+      {/* <div className="flex flex-col md:flex-row justify-center items-center md:items-start gap-10 md:gap-9 px-5 md:px-20 py-10"> */}
+      <div className="flex flex-col md:flex-row justify-center items-center md:items-start gap-10 md:gap-9 px-5 md:px-20 py-10">
+
+        <div className="flex flex-col justify-center md:items-end items-center w-full md:w-1/2 xl:w-full gap-3 order-2 md:order-1">
+          <div className="flex flex-col gap-1 w-full md:w-full text-center md:p-5 md:pt-5 lg:pt-15 p-2 md:text-left ">
+            <div className="flex items-center gap-2 md:justify-start justify-center">
+              <div className="h-px bg-gray-300 w-10 md:w-14"></div>
+              <p className="text-sm md:text-xl font-bold text-gray-300">Hello</p>
+            </div>
+            <div className="flex flex-row w-auto gap-1 md:justify-start justify-center md:gap-4 text-2xl md:text-2xl lg:text-3xl xl:text-5xl font-bold leading-tight text-gray-100">
+              <span className="">I'M</span>
+              <span className="text-purple-700">Cahya</span>
+              <span>Yoga</span>
+              <span>Ariyanto</span>
+            </div>
+            <Caption values='caption' icon="" color="" text="" style="w-full text-gray-300 text-sm md:text-base lg:text-xl opacity-80 uppercase" />
+            <Button url='https://scnmodukpcrmdwlmyozl.supabase.co/storage/v1/object/sign/project/Putih%20Minimalis%20Sederhana%20Profesional%20Administrasi%20Perkantoran%20Resume.pdf?token=eyJraWQiOiJzdG9yYWdlLXVybC1zaWduaW5nLWtleV9hM2JjYWZjMy1jMmZlLTQxNTMtOWU2Yi05N2M0MWUzNDQyOTciLCJhbGciOiJIUzI1NiJ9.eyJ1cmwiOiJwcm9qZWN0L1B1dGloIE1pbmltYWxpcyBTZWRlcmhhbmEgUHJvZmVzaW9uYWwgQWRtaW5pc3RyYXNpIFBlcmthbnRvcmFuIFJlc3VtZS5wZGYiLCJpYXQiOjE3NjU3MjE0NTQsImV4cCI6MTc5NzI1NzQ1NH0.2GE0w190sJ6Z0P2NKOOHSyKU2KbQg8962kcDIHBjLTg' />
+            <div className="flex flex-row gap-2 md:gap-5 mt-2 md:justify-start justify-center md:mt-6">
+                <Icon nama="ig" link="https://www.instagram.com/ygartn_/" color="red" size={35} />
+                <Icon nama="linkedin" link="https://www.linkedin.com/in/cahya-yoga-ariyanto-8b5139261/" color="blue" size={35} />
+                <Icon nama="github" link="https://github.com/CahyaYogaAriyanto" color="white" size={35} />
+                <Icon nama="gmail" link="https://mail.google.com/mail/?view=cm&fs=1&to=yogakecol123@gmail.com" color="red" size={35} />
+            </div>
+          </div>
+        </div>
+        {/* <div className="flex justify-end items-center"> */}
+        <div className="flex justify-center items-center order-1 md:order-2">
+          <img
+            className="w-[70%] md:w-full md:h-full drop-shadow-[0_0_3px_white] drop-shadow-[0_0_6px_white]"
+            src="/yoga.png"
+            alt="Foto Profil"
+            style={{
+              opacity: 1,
+              WebkitMaskImage: "linear-gradient(to bottom, black 50%, transparent 100%)",
+              maskImage: "linear-gradient(to bottom, black 50%, transparent 100%)",
+            }}
+          />
+        </div>
+
+      </div>
+      <SkillsSection/>
+      <EducationFlow/>
+    </>
+  );
+}
